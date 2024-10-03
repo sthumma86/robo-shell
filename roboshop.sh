62a00e59582d725c5ecf986266f57e9e69e02355
sthumma86 <sthummacld86@gmail.com>
2024-10-03 10:28:01 -0400

    Added code for ec2 creations script with tags

--- roboshop.sh
+++ roboshop.sh
@@ -12,14 +12,14 @@ for in in "${INSTANCES[@]}"
 do
     echo "Instance is: $i"
 
-    if [ $i == "mongodb" || ] [ $i == "mysql" || ] [ $i == "shiipping" ]
+    if [ $i == "mongodb" || ] [ $i == "mysql" || ] [ $i == "shipping" ]
     then 
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-01f91316b0bb612cb --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-01f91316b0bb612cb --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 
 done
 
