ff69dc3ac1d5d18a3e200b1c04b0ff4a7e9c0641
sthumma86 <sthummacld86@gmail.com>
2024-10-03 10:46:24 -0400

    Modified code for ec2 creations script with tags

--- roboshop.sh
+++ roboshop.sh
@@ -7,11 +7,11 @@
 AMI=ami-0b4f379183e5706b9
 SG_ID=sg-01f91316b0bb612cb #replace with your securitygroup id
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payments" "dispatch" "web") # array datatype
+ZONE_ID=Z01347431W3QSSXZO99H9
 
 for i in "${INSTANCES[@]}"
 do
-    echo "Instance is: $i"
-
+    
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then 
         INSTANCE_TYPE="t3.small"
@@ -20,8 +20,8 @@ do
     fi
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-01f91316b0bb612cb --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "Instance is: $i"
-    echo $i: $IP_ADDRESS
+    #echo "Instance is: $i"
+    echo $i : $IP_ADDRESS  : $INSTANCE_TYPE
 
 done
 
