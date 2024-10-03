dafc0ad805391d969e9be19ee623154e3c9b2693
sthumma86 <sthummacld86@gmail.com>
2024-10-03 10:42:04 -0400

    Modified code for ec2 creations script with tags

--- roboshop.sh
+++ roboshop.sh
@@ -19,7 +19,9 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-01f91316b0bb612cb --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-01f91316b0bb612cb --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "Instance is: $i"
+    echo $i: $IP_ADDRESS
 
 done
 
